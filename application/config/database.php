57446f27d8e52bd71a31acd065345f8b69d1592c
yo <yo@Adrian-PC>
2016-06-20 17:20:02 -0430

    Slider agregado. hay q crear imagenes

--- application/config/database.php
+++ application/config/database.php
@@ -64,11 +64,11 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' =>'localhost',// 'sainweb.ddns.net',
-	'username' =>'root',// 'sa',
-	'password' =>'',// '3Dv.*Sa.*3d_',
-	'database' =>'', //'EnterpriseTest',
-	'dbdriver' =>'mysqli',// 'sqlsrv',
+	'hostname' =>'sainweb.ddns.net',
+	'username' =>'sa',
+	'password' =>'3Dv.*Sa.*3d_',
+	'database' =>'EnterpriseTest',
+	'dbdriver' =>'sqlsrv',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => TRUE,
